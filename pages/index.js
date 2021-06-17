23c3051b1ff8c88f2de442a8d413c5c7e7d123d9
david kuseh <davegru203@gmail.com>
2021-06-17 13:38:55 +0000

    main page initial structure

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import Layout from '../src/components/Layout/Layout';
-
-export default function Home() {
-  return <Layout>main</Layout>;
-}
